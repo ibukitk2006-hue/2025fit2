a0cf9af21c3614b56596b85348de19e71345b70a
ibukitk2006-hue <ibuki.tk.2006@icloud.com>
2026-01-13 21:04:35 +0900

    Delete B-3-1.py

--- B-3-1.py
+++
@@ -1,8 +0,0 @@
-import pyxel 
-
-pyxel .init(200, 200)
-pyxel.cls(7)
-for a  in range(0, 360, 1 ):
-      c=int(a*7/360)
-      pyxel .line(100, 100, 100+100*pyxel.sin(2*a), 100+100*pyxel. cos (3*a), c)
-pyxel. show()
